078c8ad6b6750ef2cd73786e20cc6a285c7bcd84
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-09-20 19:37:34 +0200

    two elements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import * as OrbitControls from 'three-orbitcontrols';
 import { light2, light3, light4 } from './direct_light.js';
 import { mixer, action, gltfLoader2 } from './loader';
+import { mixer2, action2 } from './loader2';
 
 export const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
--- src/loader.js
+++ src/loader.js
@@ -6,7 +6,7 @@ export let mixer= new THREE.AnimationMixer();
 export let action;
 export const gltfLoader2 = new GLTFLoader()
     //gltfLoader2.load('./zawor_kulowy_three_move3.glb', (gltf) => {
-    gltfLoader2.load('./zawor_kulowy_three_kula1.glb', (gltf) => {
+    gltfLoader2.load('./zawor_kulowy_three_kula3_korpus.glb', (gltf) => {
       const root = gltf.scene;
       const anim = gltf.animations;
       scene.add(root); //co zrobic z tym?

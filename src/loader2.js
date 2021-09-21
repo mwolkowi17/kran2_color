17535e4bfe1ce22155119449dcd087c0ff299351
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-09-21 15:55:55 +0200

    color backbutton added

--- src/loader2.js
+++ src/loader2.js
@@ -24,6 +24,7 @@ export const gltfLoader2 = new GLTFLoader()
 
 const colorbutton = document.getElementById("color_button");
 
+
 colorbutton.addEventListener("click", function(){
   model_kurek="./zawor_kulowy_three_kula3_kurek_blue.glb"
   scene.remove(root);
@@ -40,5 +41,24 @@ colorbutton.addEventListener("click", function(){
 
    
   })
-  console.log("color");
 }, false);
+
+const colorbackbutton = document.getElementById("color_back_button");
+
+colorbackbutton.addEventListener("click", function(){
+  model_kurek='./zawor_kulowy_three_kula3_kurek.glb';
+  scene.remove(root);
+  gltfLoader2.load(model_kurek, (gltf) => {
+    root = gltf.scene;
+    const anim = gltf.animations;
+    //root.scale.x=2;
+    scene.add(root); //co zrobic z tym?
+    mixer2 = new THREE.AnimationMixer(root);
+    
+    action2 = mixer2.clipAction( anim[ 0 ])
+   
+    //action.play();
+
+   
+  })
+},false)

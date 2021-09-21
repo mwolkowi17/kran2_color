092725e0bc54d67877d21f855cac1c7b4a3d86d9
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-09-21 08:36:57 +0200

    color changin start

--- src/loader2.js
+++ src/loader2.js
@@ -4,9 +4,11 @@ import { scene } from './index.js';
 
 export let mixer2= new THREE.AnimationMixer();
 export let action2;
+export let model_kurek;
+model_kurek='./zawor_kulowy_three_kula3_kurek.glb';
 export const gltfLoader2 = new GLTFLoader()
     //gltfLoader2.load('./zawor_kulowy_three_move3.glb', (gltf) => {
-    gltfLoader2.load('./zawor_kulowy_three_kula3_kurek.glb', (gltf) => {
+    gltfLoader2.load(model_kurek, (gltf) => {
       const root = gltf.scene;
       const anim = gltf.animations;
       scene.add(root); //co zrobic z tym?
